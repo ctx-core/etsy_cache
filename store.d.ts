2d2de10ed2ebae0c30daa2db1132a5c1620d0e81
Brian Takita <brian.takita@gmail.com>
2020-01-04 19:03:32 -0500

    feat: store: b__ factories

--- store.d.ts
+++ store.d.ts
@@ -1,16 +1,26 @@
-export declare const __a1__listing__etsy__s3__loading: import("svelte/store").Writable<any>;
-export declare const __arr__listing__etsy__s3__loading: import("svelte/store").Writable<any>;
-export declare function reload__a1__listing__etsy__s3__loading(): Promise<void>;
-export declare const reload__arr__listing__etsy__s3__loading: typeof reload__a1__listing__etsy__s3__loading;
-export declare const __a1__images__listing__etsy__s3__loading: import("svelte/store").Writable<any>;
-export declare const __arr__images__listing__etsy__s3__loading: import("svelte/store").Writable<any>;
-export declare function reload__a1__images__listing__etsy__s3__loading(): Promise<void>;
-export declare const reload__arr__images__listing__etsy__s3__loading: typeof reload__a1__images__listing__etsy__s3__loading;
-export declare const __ctx__listing__etsy: import("svelte/store").Readable<{
-    a1__listing__etsy__s3: any;
-    a1__images__listing__etsy__s3: any;
+import { Writable } from 'svelte/store';
+import { falsy } from '@ctx-core/function';
+export interface Writable__a1__listing__etsy__s3__loading extends Writable<falsy | any[]> {
+    reload__a1__listing__etsy__s3__loading: () => void;
+}
+export declare const b__a1__listing__etsy__s3__loading: (ctx?: any, opts?: any) => Writable__a1__listing__etsy__s3__loading;
+export declare const __a1__listing__etsy__s3__loading: Writable__a1__listing__etsy__s3__loading;
+export declare const reload__a1__listing__etsy__s3__loading: () => void;
+export interface Writable__reload__a1__images__listing__etsy__s3__loading extends Writable<falsy | string[]> {
+    reload__a1__images__listing__etsy__s3__loading: () => void;
+}
+export declare const b__a1__images__listing__etsy__s3__loading: (ctx?: any, opts?: any) => Writable__reload__a1__images__listing__etsy__s3__loading;
+export declare const __a1__images__listing__etsy__s3__loading: Writable__reload__a1__images__listing__etsy__s3__loading;
+export declare const reload__a1__images__listing__etsy__s3__loading: () => void;
+export declare const b__ctx__listing__etsy: (ctx?: any, opts?: any) => import("svelte/store").Readable<{
+    a1__listing__etsy__s3: any[];
+    a1__images__listing__etsy__s3: string[];
 }>;
+export declare const __ctx__listing__etsy: import("svelte/store").Readable<{
+    a1__listing__etsy__s3: any[];
+    a1__images__listing__etsy__s3: string[];
+}>;
+export declare const b__a1__listing__etsy__s3: (ctx?: any, opts?: any) => import("svelte/store").Readable<any>;
 export declare const __a1__listing__etsy__s3: import("svelte/store").Readable<any>;
-export declare const __arr__listing__etsy__s3: import("svelte/store").Readable<any>;
+export declare const b__a1__images__listing__etsy__s3: (ctx?: any, opts?: any) => import("svelte/store").Readable<any>;
 export declare const __a1__images__listing__etsy__s3: import("svelte/store").Readable<any>;
-export declare const __arr__images__listing__etsy__s3: import("svelte/store").Readable<any>;
--- store.ts
+++ store.ts
@@ -1,44 +1,74 @@
-import { writable, derived } from 'svelte/store'
-import { _andand } from '@ctx-core/function'
+import { writable, derived, Writable } from 'svelte/store'
+import { _b, assign } from '@ctx-core/object'
+import { _andand, falsy } from '@ctx-core/function'
 import {
 	_a1__listing__etsy__s3,
 	_a1__images__listing__etsy__s3,
 } from './fetch'
 import { _has__dom } from '@ctx-core/dom'
-export const __a1__listing__etsy__s3__loading = writable(null)
-if (_has__dom()) {
-	reload__a1__listing__etsy__s3__loading()
+export interface Writable__a1__listing__etsy__s3__loading extends Writable<falsy|any[]> {
+	reload__a1__listing__etsy__s3__loading:()=>void
 }
-export const __arr__listing__etsy__s3__loading = __a1__listing__etsy__s3__loading
-export async function reload__a1__listing__etsy__s3__loading() {
-	const arr__listing__etsy__s3 = await _a1__listing__etsy__s3()
-	__a1__listing__etsy__s3__loading.set(arr__listing__etsy__s3)
+export const b__a1__listing__etsy__s3__loading = _b<Writable__a1__listing__etsy__s3__loading>('__a1__listing__etsy__s3__loading', ()=>{
+	const __a1__listing__etsy__s3__loading = writable(null)
+	if (_has__dom()) {
+		reload__a1__listing__etsy__s3__loading()
+	}
+	return assign(__a1__listing__etsy__s3__loading, {
+		reload__a1__listing__etsy__s3__loading,
+	})
+	async function reload__a1__listing__etsy__s3__loading() {
+		const arr__listing__etsy__s3 = await _a1__listing__etsy__s3()
+		__a1__listing__etsy__s3__loading.set(arr__listing__etsy__s3)
+	}
+})
+export const __a1__listing__etsy__s3__loading = b__a1__listing__etsy__s3__loading()
+export const {
+	reload__a1__listing__etsy__s3__loading,
+} = __a1__listing__etsy__s3__loading
+export interface Writable__reload__a1__images__listing__etsy__s3__loading extends Writable<falsy|string[]> {
+	reload__a1__images__listing__etsy__s3__loading:()=>void
 }
-export const reload__arr__listing__etsy__s3__loading = reload__a1__listing__etsy__s3__loading
-export const __a1__images__listing__etsy__s3__loading = writable(null)
-export const __arr__images__listing__etsy__s3__loading = __a1__images__listing__etsy__s3__loading
-if (_has__dom()) {
-	reload__a1__images__listing__etsy__s3__loading()
-}
-export async function reload__a1__images__listing__etsy__s3__loading() {
-	const arr__images__listing__etsy__s3 = await _a1__images__listing__etsy__s3()
-	__a1__images__listing__etsy__s3__loading.set(arr__images__listing__etsy__s3)
-}
-export const reload__arr__images__listing__etsy__s3__loading = reload__a1__images__listing__etsy__s3__loading
-export const __ctx__listing__etsy = derived([
-		__a1__listing__etsy__s3__loading,
-		__a1__images__listing__etsy__s3__loading
-	],
-	([a1__listing__etsy__s3, a1__images__listing__etsy__s3]) =>
-		a1__listing__etsy__s3
-		&& a1__images__listing__etsy__s3
-		&& {
-			a1__listing__etsy__s3,
-			a1__images__listing__etsy__s3,
-		})
-export const __a1__listing__etsy__s3 =
-	derived(__ctx__listing__etsy, _andand('a1__listing__etsy__s3'))
-export const __arr__listing__etsy__s3 = __a1__listing__etsy__s3
-export const __a1__images__listing__etsy__s3 =
-	derived(__ctx__listing__etsy, _andand('a1__images__listing__etsy__s3'))
-export const __arr__images__listing__etsy__s3 = __a1__images__listing__etsy__s3
+export const b__a1__images__listing__etsy__s3__loading = _b<Writable__reload__a1__images__listing__etsy__s3__loading>('__a1__images__listing__etsy__s3__loading', ()=>{
+	const __a1__images__listing__etsy__s3__loading = writable(null)
+	if (_has__dom()) {
+		reload__a1__images__listing__etsy__s3__loading()
+	}
+	return assign(__a1__images__listing__etsy__s3__loading, {
+		reload__a1__images__listing__etsy__s3__loading,
+	})
+	async function reload__a1__images__listing__etsy__s3__loading() {
+		const a1__images__listing__etsy__s3 =
+			await _a1__images__listing__etsy__s3()
+		__a1__images__listing__etsy__s3__loading.set(
+			a1__images__listing__etsy__s3)
+	}
+})
+export const __a1__images__listing__etsy__s3__loading = b__a1__images__listing__etsy__s3__loading()
+export const {
+	reload__a1__images__listing__etsy__s3__loading,
+} = __a1__images__listing__etsy__s3__loading
+export const b__ctx__listing__etsy = _b('__ctx__listing__etsy', ctx=>
+	derived(
+		[
+			b__a1__listing__etsy__s3__loading(ctx),
+			b__a1__images__listing__etsy__s3__loading(ctx),
+		],
+		([a1__listing__etsy__s3, a1__images__listing__etsy__s3])=>
+			a1__listing__etsy__s3
+			&& a1__images__listing__etsy__s3
+			&& {
+				a1__listing__etsy__s3,
+				a1__images__listing__etsy__s3,
+			}))
+export const __ctx__listing__etsy = b__ctx__listing__etsy()
+export const b__a1__listing__etsy__s3 = _b('__a1__listing__etsy__s3', ctx=>
+	derived(
+		b__ctx__listing__etsy(ctx),
+		_andand('a1__listing__etsy__s3')))
+export const __a1__listing__etsy__s3 = b__a1__listing__etsy__s3()
+export const b__a1__images__listing__etsy__s3 = _b('__a1__images__listing__etsy__s3', ctx=>
+	derived(
+		b__ctx__listing__etsy(ctx),
+		_andand('a1__images__listing__etsy__s3')))
+export const __a1__images__listing__etsy__s3 = b__a1__images__listing__etsy__s3()
